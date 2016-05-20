f3967e6342326c16f292fddcac7144be7b175328
JustCarmen <carmen@justcarmen.nl>
2016-05-20 23:19:35 +0200

    webtrees has introduced new class constants for custom module version and website. Change our custom module definitions into webtrees defaults.

--- module.php
+++ module.php
@@ -28,10 +28,11 @@ use Fisharebest\Webtrees\Module\ModuleMenuInterface;
 use Fisharebest\Webtrees\Theme;
 use JustCarmen\WebtreesAddOns\FancyImagebar\Template\AdminTemplate;
 
-define('FIB_VERSION', '1.7.5-dev');
-
 class FancyImagebarModule extends AbstractModule implements ModuleConfigInterface, ModuleMenuInterface {
-
+	
+	const CUSTOM_VERSION = '1.7.5-dev';
+	const CUSTOM_WEBSITE = 'http://www.justcarmen.nl/fancy-modules/fancy-imagebar/';
+	
 	/** @var string location of the Fancy imagebar module files */
 	var $directory;
 
@@ -62,7 +63,7 @@ class FancyImagebarModule extends AbstractModule implements ModuleConfigInterfac
 
 	// Extend Module
 	public function getDescription() {
-		return /* I18N: Description of the module */ I18N::translate('An imagebar with small images between header and content.') . '<br><span class="small text-muted">' . I18N::translate('Version') . ' ' . FIB_VERSION . ' | by JustCarmen | <a href="http://www.justcarmen.nl/fancy-modules/fancy-imagebar/">' . I18N::translate('Show details') . '</a></span>';
+		return /* I18N: Description of the module */ I18N::translate('An imagebar with small images between header and content.');
 	}
 
 	// Extend ModuleConfigInterface

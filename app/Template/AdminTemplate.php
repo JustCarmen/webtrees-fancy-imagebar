9100df8a7df82362751d527b878c0a464e610999
JustCarmen <carmen@justcarmen.nl>
2017-06-26 21:35:35 +0200

    short array syntax

--- app/Template/AdminTemplate.php
+++ app/Template/AdminTemplate.php
@@ -278,7 +278,7 @@ class AdminTemplate extends FancyImagebarClass {
                   <?= I18N::translate('Images Tone') ?>
                 </label>
                 <div class="col-sm-2">
-                  <?= Bootstrap4::select(array('Sepia', 'Black and White', 'Colors'), $this->options('tone'), ['name' => 'NEW_FIB_OPTIONS[TONE]']) ?>
+                  <?= Bootstrap4::select(['Sepia', 'Black and White', 'Colors'], $this->options('tone'), ['name' => 'NEW_FIB_OPTIONS[TONE]']) ?>
                 </div>
               </div>
               <!-- SEPIA -->

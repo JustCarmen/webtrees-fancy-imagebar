26c11809aafdc0f80e894f97440d6022d542a71a
JustCarmen <carmen@justcarmen.nl>
2017-12-10 20:21:11 +0100

    new admin route url

--- module.php
+++ module.php
@@ -114,9 +114,12 @@ class FancyImagebarModule extends AbstractModule implements ModuleConfigInterfac
 	}
 	}
 
-	// Implement ModuleConfigInterface
+	/** {@inheritdoc} */
 	public function getConfigLink() {
-		return 'module.php?mod=' . $this->getName() . '&amp;mod_action=admin_config';
+		return Html::url('module.php', [
+			'mod'        => $this->getName(),
+			'mod_action' => 'admin_config',
+		]);
 	}
 
 	// Implement ModuleMenuInterface

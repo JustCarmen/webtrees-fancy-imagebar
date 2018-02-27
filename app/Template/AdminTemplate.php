9d29c555c78dbc682cd0537b329fd00dfcb565d7
JustCarmen <carmen@justcarmen.nl>
2018-02-27 07:35:12 +0100

    Fix admin breadcrumb

--- app/Template/AdminTemplate.php
+++ app/Template/AdminTemplate.php
@@ -174,9 +174,8 @@ class AdminTemplate extends FancyImagebarClass {
 		global $WT_TREE;
 
 		echo Bootstrap4::breadcrumbs([
-			route('admin-control-panel')                                      => I18N::translate('Control panel'),
-			route('admin-modules')                                            => I18N::translate('Module administration'),
-			'module.php?mod=' . $this->getName() . '&mod_action=admin_config' => $this->getTitle(),
+			route('admin-control-panel') => I18N::translate('Control panel'),
+			route('admin-modules')       => I18N::translate('Module administration'),
 		], $controller->getPageTitle()); ?>
 
 		<div class="fancy-imagebar-admin">

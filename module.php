26a484b826872630ca98b6d83058ba435ea3b12b
JustCarmen <carmen@justcarmen.nl>
2018-03-01 07:42:05 +0100

    Concatenate script

--- module.php
+++ module.php
@@ -166,13 +166,13 @@ class FancyImagebarModule extends AbstractModule implements ModuleConfigInterfac
 	 */
 	protected function includeCss() {
 		return
-		'<script>
-          var newSheet=document.createElement("link");
-          newSheet.setAttribute("rel","stylesheet");
-          newSheet.setAttribute("type","text/css");
-          newSheet.setAttribute("href","' . $this->directory . '/css/style.css");
-          document.getElementsByTagName("head")[0].appendChild(newSheet);
-        </script>';
+		'<script>' .
+		  'var newSheet=document.createElement("link"); ' .
+		  'newSheet.setAttribute("rel","stylesheet"); ' .
+		  'newSheet.setAttribute("type","text/css"); ' .
+		  'newSheet.setAttribute("href","' . $this->directory . '/assets/css/style.css"); ' .
+		  'document.getElementsByTagName("head")[0].appendChild(newSheet); ' .
+		'</script>';
 	}
 }
 

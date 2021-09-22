583f8c303ff2702fb63130f5067a46af60fe7dbf
JustCarmen <carmen@justcarmen.nl>
2021-09-22 21:57:29 +0200

    Bump dev-version

--- FancyImagebarModule.php
+++ FancyImagebarModule.php
@@ -89,7 +89,7 @@ class FancyImagebarModule extends AbstractModule implements ModuleCustomInterfac
      */
     public function customModuleVersion(): string
     {
-        return '2.1.1';
+        return '2.1.2-dev';
     }
 
     /**

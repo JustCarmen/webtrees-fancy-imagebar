30143009b8c2b5a6fbc2c87120e81a711663e4cd
JustCarmen <carmen@justcarmen.nl>
2022-09-10 08:33:14 +0200

    Disable mix-manifest file from laravel entry point

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,12 @@
  * Load the appropriate section
  */
 
- if (process.env.section) {
+if (process.env.section) {
   require(`${__dirname}/webpack.mix.${process.env.section}.js`);
 }
+
+let mix = require('laravel-mix');
+
+// Disable mix-manifest.json (https://github.com/laravel-mix/laravel-mix/issues/580#issuecomment-919102692)
+// Prevent the distribution zip file from containing an unwanted file
+mix.options({ manifest: false });
